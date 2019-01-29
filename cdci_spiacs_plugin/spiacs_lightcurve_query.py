bd62b12dd23f4a461134da9b1a7387d039fba72c
andrea tramacere <orion@MacBook-Pro-3.local>
2019-01-29 15:17:40 +0100

    debug

--- cdci_spiacs_plugin/spiacs_lightcurve_query.py
+++ cdci_spiacs_plugin/spiacs_lightcurve_query.py
@@ -134,8 +134,8 @@ class SpicasLigthtCurve(LightCurveProduct):
 
         if delta_t is not None and delta_t>instr_t_bin:
 
-            t1=data['time'][0]-instr_t_bin*0.5
-            t2=data['time'][-1]+instr_t_bin*0.5
+            t1=data['time'][0]
+            t2=data['time'][-1]+instr_t_bin
             n_bins=int((t2-t1)/delta_t)
             binned_data = np.zeros(n_bins, dtype=[('rate', '<f8'), ('rate_err', '<f8'), ('time', '<f8')])
             digitized_ids =np.digitize(data['time'],np.linspace(t1,t2,n_bins))
