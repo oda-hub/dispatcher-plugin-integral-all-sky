1457d5622cd6edca45183ad1a57e920da2e9fc44
andrea tramacere <orion@MacBook-Pro-3.local>
2019-01-29 13:57:51 +0100

    debug

--- cdci_spiacs_plugin/spiacs_lightcurve_query.py
+++ cdci_spiacs_plugin/spiacs_lightcurve_query.py
@@ -179,9 +179,9 @@ class SpiacsLightCurveQuery(LightCurveQuery):
         T2=instrument.get_par_by_name('T2')._astropy_time
 
         delta_t=T2-T1
-        T_ref=(T2+T1)*.5
+        T_ref=time.Time((T2.mjd + T1.mjd) * 0.5, format='mjd').isot
         t_bin = instrument.get_par_by_name('time_bin')._astropy_time_delta.sec
-        param_dict=self.set_instr_dictionaries(T_ref.iso,delta_t.sec)
+        param_dict=self.set_instr_dictionaries(T_ref,delta_t.sec)
 
         print ('build here',config,instrument)
         q = SpiacsDispatcher(instrument=instrument,config=config,param_dict=param_dict)
