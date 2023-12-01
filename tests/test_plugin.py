8d6846884440e242a072803e95cb54357b8eab7d
burnout87 <gabriele.barni@gmail.com>
2023-12-01 14:52:13 +0100

    addressing pep8speaks comment

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -197,8 +197,10 @@ def test_request_too_large(dispatcher_live_fixture):
     assert c.status_code == 200
 
     assert jdata['job_status'] == 'failed'
-    assert 'SPI-ACS backend refuses to process this request' in jdata['exit_status']['debug_message']
-    assert 'Instrument: spi_acs, product: spi_acs_lc' in jdata['exit_status']['error_message']
+    assert ('SPI-ACS backend refuses to process this request' in
+            jdata['exit_status']['debug_message'])
+    assert ('Instrument: spi_acs, product: spi_acs_lc' in
+            jdata['exit_status']['error_message'])
 
 
 @pytest.mark.parametrize("roles", ["integral-realtime", ""])
